d97afa9c3a1655aba309696353087a0899bfc1b1
Saptarshi Patra <saptarshi.patra.22@gmail.com>
2022-04-26 22:52:12 +0530

    Inconsistency Lines Removed Line is now more Straight Line

--- lanes.py
+++ lanes.py
@@ -1,6 +1,16 @@
 import cv2
 import numpy as np
 
+def make_coordinates(image, line_parametres):
+    slope, intercept=line_parametres
+    #print(image.shape)
+    y1=image.shape[0]
+    y2=int(y1*(3/5))
+    #y=mx+b => x=(y-b)/m
+    x1=int((y1-intercept)/slope)
+    x2=int((y2-intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
 def average_slope_intercept(image, lines):
     left_fit=[]
     right_fit=[]
@@ -18,8 +28,11 @@ def average_slope_intercept(image, lines):
     # print(right_fit)
     left_fit_average=np.average(left_fit, axis=0)
     right_fit_average=np.average(right_fit, axis=0)
-    print(left_fit_average, 'left')
-    print(right_fit_average, 'right')
+    # print(left_fit_average, 'left')
+    # print(right_fit_average, 'right')
+    left_line=make_coordinates(image, left_fit_average)
+    right_line=make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
 
 def canny(image):
     gray=cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -49,7 +62,7 @@ canny_image=canny(lane_image)
 cropped_image=region_of_interest(canny_image)
 lines=cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
 averaged_lines=average_slope_intercept(lane_image, lines)
-line_image=display_lines(lane_image, lines)
+line_image=display_lines(lane_image, averaged_lines)
 combo_image=cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-cv2.imshow('reasult', combo_image)
+cv2.imshow('reasult', line_image)
 cv2.waitKey(0)
