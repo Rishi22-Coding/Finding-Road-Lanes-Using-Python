cba7592c0de0a0c821ede6a6ed8b49a6c1ee3423
Saptarshi Patra <saptarshi.patra.22@gmail.com>
2022-04-26 22:43:28 +0530

    Single line left and right array generated for Optimization

--- lanes.py
+++ lanes.py
@@ -1,8 +1,28 @@
 import cv2
 import numpy as np
 
+def average_slope_intercept(image, lines):
+    left_fit=[]
+    right_fit=[]
+    for line in lines:
+        x1, y1, x2, y2=line.reshape(4)
+        parametres=np.polyfit((x1, x2), (y1, y2), 1)
+        # print(parametres)
+        slope=parametres[0]
+        intercept=parametres[1]
+        if slope<0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    # print(left_fit)
+    # print(right_fit)
+    left_fit_average=np.average(left_fit, axis=0)
+    right_fit_average=np.average(right_fit, axis=0)
+    print(left_fit_average, 'left')
+    print(right_fit_average, 'right')
+
 def canny(image):
-    gray=cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    gray=cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur=cv2.GaussianBlur(gray, (5,5),0)
     canny=cv2.Canny(blur, 50, 150)
     return canny
@@ -25,9 +45,10 @@ def region_of_interest(image):
 
 image =cv2.imread('./img/test_image.jpg')
 lane_image=np.copy(image)
-canny=canny(lane_image)
-cropped_image=region_of_interest(canny)
+canny_image=canny(lane_image)
+cropped_image=region_of_interest(canny_image)
 lines=cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+averaged_lines=average_slope_intercept(lane_image, lines)
 line_image=display_lines(lane_image, lines)
 combo_image=cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 cv2.imshow('reasult', combo_image)
