dfbc3941447efa03a3aee85cc86f36847cfbad0b
Saptarshi Patra <saptarshi.patra.22@gmail.com>
2022-04-27 11:59:51 +0530

    Completed to Present the Project

--- lanes.py
+++ lanes.py
@@ -1,8 +1,5 @@
 import cv2
 import numpy as np
-from cv2 import VideoCapture
-from cv2 import waitKey
-
 def make_coordinates(image, line_parametres):
     slope, intercept=line_parametres
     #print(image.shape)
@@ -12,7 +9,6 @@ def make_coordinates(image, line_parametres):
     x1=int((y1-intercept)/slope)
     x2=int((y2-intercept)/slope)
     return np.array([x1, y1, x2, y2])
-
 def average_slope_intercept(image, lines):
     left_fit=[]
     right_fit=[]
@@ -35,13 +31,11 @@ def average_slope_intercept(image, lines):
     left_line=make_coordinates(image, left_fit_average)
     right_line=make_coordinates(image, right_fit_average)
     return np.array([left_line, right_line])
-
 def canny(image):
     gray=cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur=cv2.GaussianBlur(gray, (5,5),0)
     canny=cv2.Canny(blur, 50, 150)
     return canny
-
 def display_lines(image, lines):
     line_image=np.zeros_like(image)
     if lines is not None:
@@ -49,7 +43,6 @@ def display_lines(image, lines):
             x1, y1, x2, y2=line.reshape(4)
             cv2.line(line_image, (x1, y1), (x2,y2), (255, 0, 0), 10)
     return line_image
-
 def region_of_interest(image):
     height=image.shape[0]
     polygons=np.array([[(200, height), (1100, height), (550, 250)]])
@@ -57,9 +50,7 @@ def region_of_interest(image):
     cv2.fillPoly(mask, polygons, 255)
     masked_image=cv2.bitwise_and(image, mask)
     return masked_image
-
-#Algo for image
-image =cv2.imread("./img/test_image.jpg")
+image =cv2.imread('./img/test_image.jpg')
 lane_image=np.copy(image)
 canny_image=canny(lane_image)
 cropped_image=region_of_interest(canny_image)
@@ -67,20 +58,7 @@ lines=cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLen
 averaged_lines=average_slope_intercept(lane_image, lines)
 line_image=display_lines(lane_image, averaged_lines)
 combo_image=cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('reasult', line_image)
 #cv2.imshow('reasult', line_image)
 cv2.imshow('reasult', combo_image)
 cv2.waitKey(0)
-
-#Same Algo For Video
-# capt=cv2.videoCapture('./vid/test2.mp4')
-# while(capt.isOpened()):
-#     _, frame=capt.read()
-#     canny_image=canny(lane_vid)
-#     cropped_image=region_of_interest(canny_image)
-#     lines=cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-#     averaged_lines=average_slope_intercept(lane_image, lines)
-#     line_image=display_lines(lane_vid, averaged_lines)
-#     combo_image=cv2.addWeighted(lane_vid, 0.8, line_image, 1, 1)
-#     #cv2.imshow('reasult', line_image)
-#     cv2.imshow('reasult', combo_image)
-#     cv2.waitKey(1)
